dd58c29e0f158561b01c480bc8d9ca3741ca11b1
Prateek Singh <prateeksingh19460@gmail.com>
2026-04-27 21:42:42 +0530

    same port realtime updation done

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ import express from "express";
 //pnpm install socket.io
 import { Server } from "socket.io";
 
+const CHECKBOX_SIZE = 100;
+
+const state = {
+    checkboxes: new Array(CHECKBOX_SIZE).fill(false)
+}
+
 async function main(){
     const PORT = process.env.PORT ?? 8000;
 
@@ -19,14 +25,21 @@ async function main(){
         console.log(`Socket connected`,{id:socket.id})
 
         socket.on('client:checkbox:change',(data)=>{
-            console.log(`[Socket:${socket.id}:client:checkbox:change]`,data)
+            console.log(`[Socket:${socket.id}]:client:checkbox:change`,data);
+            io.emit('server:checkbox:change',data) // broadcast to all clients
+            state.checkboxes[data.index]= data.checked;
         });
     })
     //express
+
     app.use(express.static(path.resolve('./public')));
     app.get('/health', (req,res)=>{
         res.json({ healthy: true })
     })
+    app.get('/checkboxes', (req,res)=>{
+        res.json({ checkboxes: state.checkboxes })
+    })
+    // fetch('/checkboxes')
     server.listen(PORT,()=>{
         console.log(`server is running on PORT http://localhost:${PORT}`)
     }) 
